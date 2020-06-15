9640bdd7a9602e7ba206c6e27d0e8502b20acbca
YOU54F <yousafn@gmail.com>
2020-06-15 11:52:20 +0100

    complete initial example with koa, forwarding request to pact stub service with axios

--- src/functions/router.ts
+++ src/functions/router.ts
@@ -1,17 +1,14 @@
-import koa from 'koa'
-import http from 'koa-route'
+import koa from "koa";
 import pino from "pino";
 import axios, { AxiosError } from "axios";
 import { spawnPactServerAndWait, PactServerResult } from "./utils/pact";
 
-export const Router = new koa()
+export const Router = new koa();
 
-
-const handler = async (ctx: koa.Context) => {
-
+Router.use(async (ctx) => {
+  
   const dest = pino.destination({ sync: false });
   const logger = pino(dest).child({
-    // ctx,
   });
   let pactMockRes: PactServerResult;
   try {
@@ -19,24 +16,26 @@ const handler = async (ctx: koa.Context) => {
     if (pactMockRes.started) {
       logger.info(
         {
+          ctxReq: ctx.request,
+          ctxRes: ctx.response,
           pactProcess: pactMockRes.started,
           msg: "Pact stub service spawned and ready for request",
         },
         pactMockRes.pactProcessLog.join("")
       );
+
       const result = await axios
-        .get("http://localhost:9999/v2/pet/1845563262948980200", {})
-        .then();
-      logger.info({ result: result.data }, "Pact Stub Service response");
-      // return ok(result.data);
+      .get(`http://localhost:9999${ctx.request.url}`, {})
+      .then();
 
+      logger.info({ result: result.data }, "Pact Stub Service Response");
+  
       ctx.body = result.data
-
     }
   } catch (error) {
     if (error && error.isAxiosError) {
       const axiosError = error as AxiosError;
-
+  
       if (error.response) {
         logger.error(
           {
@@ -47,7 +46,7 @@ const handler = async (ctx: koa.Context) => {
         );
         throw axiosError.response.data;
       }
-
+  
       logger.error(
         {
           error: axiosError.message,
@@ -57,7 +56,7 @@ const handler = async (ctx: koa.Context) => {
       );
       throw axiosError.message;
     }
-
+  
     if (!pactMockRes) {
       logger.error(
         {
@@ -69,7 +68,7 @@ const handler = async (ctx: koa.Context) => {
       throw new Error(error);
     }
     const errorMessageGeneric = "Generic error occurred";
-
+  
     logger.error(
       {
         error: error.toString(),
@@ -79,9 +78,11 @@ const handler = async (ctx: koa.Context) => {
     );
     throw new Error(error.toString());
   } finally {
-    pactMockRes.pactStubProcess.kill()
+    pactMockRes.pactStubProcess.kill();
     dest.flushSync();
   }
-};
 
-Router.use(http.get('*', handler))
+});
+
+
+
--- src/functions/utils/pact.ts
+++ src/functions/utils/pact.ts
@@ -1,4 +1,8 @@
-import { spawn, ChildProcessByStdio, ChildProcessWithoutNullStreams } from "child_process";
+import {
+  spawn,
+  ChildProcessByStdio,
+  ChildProcessWithoutNullStreams,
+} from "child_process";
 const { chunksToLinesAsync } = require("@rauschma/stringio");
 import pino from "pino";
 import internal from "stream";
@@ -17,19 +21,49 @@ export const spawnPactServerAndWait = async (
     try {
       logger.info("Calling pact mock server");
       let args = ["pact-stub-service"];
+      args.push(`--log-level`);
+      args.push(`DEBUG`);
       args.push(`--host`);
       args.push(`0.0.0.0`);
       args.push(`--port`);
       args.push(`9999`);
       args.push(`pact.json`);
-      logger.info(args)
+
+//       Usage:
+//   pact-stub-service PACT_URI ...
+
+// Options:
+//   -p, [--port=PORT]                        # Port on which to run the service
+//   -h, [--host=HOST]                        # Host on which to bind the service
+//                                            # Default: localhost
+//   -l, [--log=LOG]                          # File to which to log output
+//   -n, [--broker-username=BROKER_USERNAME]  # Pact Broker basic auth username
+//   -p, [--broker-password=BROKER_PASSWORD]  # Pact Broker basic auth password
+//   -k, [--broker-token=BROKER_TOKEN]        # Pact Broker bearer token (can also be set using the PACT_BROKER_TOKEN environment variable)
+//       [--log-level=LOG_LEVEL]              # Log level. Options are DEBUG INFO WARN ERROR
+//                                            # Default: DEBUG
+//   -o, [--cors=CORS]                        # Support browser security in tests by responding to OPTIONS requests and adding CORS headers to mocked responses
+//       [--ssl], [--no-ssl]                  # Use a self-signed SSL cert to run the service over HTTPS
+//       [--sslcert=SSLCERT]                  # Specify the path to the SSL cert to use when running the service over HTTPS
+//       [--sslkey=SSLKEY]                    # Specify the path to the SSL key to use when running the service over HTTPS
+
+// Description:
+//   Start a stub service with the given pact file(s) or directories. Pact URIs may be local file or directory paths, or HTTP.
+//   Include any basic auth details in the URL using the format https://USERNAME:PASSWORD@URI. Where multiple matching
+//   interactions are found, the interactions will be sorted by response status, and the first one will be returned. This
+//   may lead to some non-deterministic behaviour. If you are having problems with this, please raise it on the pact-dev
+//   google group, and we can discuss some potential enhancements. Note that only versions 1 and 2 of the pact
+//   specification are currently fully supported. Pacts using the v3 format may be used, however, any matching features
+//   added in v3 will currently be ignored.
       const pactStubProcess = spawn("/bin/sh", ["-c", args.join(" ")]);
       pactStubProcess.stderr.on("data", (data) => {
         errorMessage = data.toString();
       });
+      pactStubProcess.stdout.on("data", (data) => {
+        pactProcessLog.push(data);
+      });
       const started = await echoReadable(
-        pactStubProcess.stdout,
-        pactProcessLog
+        pactStubProcess.stdout
       );
       if (!started) {
         return reject(`Pact Stub Service: ${errorMessage}`);
@@ -37,22 +71,22 @@ export const spawnPactServerAndWait = async (
         return resolve({
           started,
           pactProcessLog,
-          pactStubProcess
+          pactStubProcess,
         });
       }
     } catch (e) {
-      throw new Error("An error occurred whilst calling the pact mock service")
+      throw new Error("An error occurred whilst calling the pact mock service");
     }
   });
 };
 
 async function echoReadable(
   readable: internal.Readable,
-  pactProcessLog: string[]
+  // pactProcessLog: string[]
 ) {
   let started = false;
   for await (const line of chunksToLinesAsync(readable)) {
-    pactProcessLog.push(line);
+    // pactProcessLog.push(line);
     if (line.includes("WEBrick::HTTPServer#start: pid=")) {
       return (started = true);
     }
