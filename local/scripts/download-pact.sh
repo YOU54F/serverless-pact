744557622d18468acef769f910afdb7584d6c60d
YOU54F <yousafn@gmail.com>
2020-06-21 02:22:31 +0100

    dont remove apt list

--- local/scripts/download-pact.sh
+++ local/scripts/download-pact.sh
@@ -6,7 +6,6 @@ export PACT_RUBY_VERSION="1.66.0"
 
 curl -L -O https://github.com/pact-foundation/pact-ruby-standalone/releases/download/v${PACT_RUBY_VERSION}/pact-${PACT_RUBY_VERSION}-linux-x86_64.tar.gz && \
     tar -xf pact-${PACT_RUBY_VERSION}-linux-x86_64.tar.gz && \
-    rm pact-${PACT_RUBY_VERSION}-linux-x86_64.tar.gz && \
-    rm -rf /var/lib/apt/lists/*
+    rm pact-${PACT_RUBY_VERSION}-linux-x86_64.tar.gz
 
 export PATH="/opt/pact/bin/:${PATH}"
