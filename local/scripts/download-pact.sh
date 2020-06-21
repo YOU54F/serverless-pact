fdfba54de996e6a80c571b055dca4b0381cddc51
YOU54F <yousafn@gmail.com>
2020-06-21 02:18:23 +0100

    set PACT_RUBY_VERSION="1.66.0"

--- local/scripts/download-pact.sh
+++ local/scripts/download-pact.sh
@@ -2,6 +2,8 @@
 
 cd /opt
 
+export PACT_RUBY_VERSION="1.66.0" 
+
 curl -L -O https://github.com/pact-foundation/pact-ruby-standalone/releases/download/v${PACT_RUBY_VERSION}/pact-${PACT_RUBY_VERSION}-linux-x86_64.tar.gz && \
     tar -xf pact-${PACT_RUBY_VERSION}-linux-x86_64.tar.gz && \
     rm pact-${PACT_RUBY_VERSION}-linux-x86_64.tar.gz && \
